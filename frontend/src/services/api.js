7b5ae5c670fa00a6bab39ca80ea5d710778ddfb0
Suds Kumar <suds123@gmail.com>
2025-10-21 08:40:24 +0100

    Create api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const clientsAPI = {
+  getAll: (params = {}) => api.get('/clients', { params }),
+  getProfile: (clientId) => api.get(`/clients/${clientId}/profile`),
+  getTimeline: (clientId) => api.get(`/clients/${clientId}/timeline`),
+  getInsights: (clientId) => api.get(`/clients/${clientId}/insights`),
+  getMedia: (clientId) => api.get(`/clients/${clientId}/media`),
+};
+
+export const actionsAPI = {
+  create: (action) => api.post('/actions', action),
+  get: (actionId) => api.get(`/actions/${actionId}`),
+};
+
+export default api;
