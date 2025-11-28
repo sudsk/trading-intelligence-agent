340c58fd115c9121e7eb97d5b18a7278030be563
Suds Kumar <suds123@gmail.com>
2025-11-28 00:16:57 +0000

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1',
+  baseURL: `${process.env.REACT_APP_API_URL || 'http://localhost:8000'}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
   },
