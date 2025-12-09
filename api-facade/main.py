e3dd6d9a1ee8b4b3c58d3aeb9107d1df5c0487e3
Suds Kumar <suds123@gmail.com>
2025-12-09 09:42:46 +0000

    Update main.py

--- api-facade/main.py
+++ api-facade/main.py
@@ -22,7 +22,11 @@ from services.agent_client import AgentClient
 from services.alert_queue import AlertQueue
 from services.data_service import DataService
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 logger = logging.getLogger(__name__)
 
 
