6d1e13e40566f64295a8cbc93a6e896b28926799
Suds Kumar <suds123@gmail.com>
2025-12-16 13:17:49 +0000

    Update AlertBanner.js

--- frontend/src/components/AlertBanner.js
+++ frontend/src/components/AlertBanner.js
@@ -18,7 +18,7 @@ const AlertBanner = ({ alert, onDismiss }) => {
         </div>
         <button 
           className="action-btn btn-primary" 
-          style={{ width: 'auto', flex: 0, marginTop: '-8px' }}
+          style={{ width: 'auto',}}
           onClick={onDismiss}
         >
           Take Action
